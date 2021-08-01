ffe7bc8775961f106f6404dbe5b7a126cfc5807a
z0mb13-1705 <66255337+z0mb13-1705@users.noreply.github.com>
2021-08-02 01:02:50 +0700

    Add files via upload

--- code_challenge_1.cpp
+++ code_challenge_1.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> grayCode(int n) {
         vector<int> res;
         res.push_back(0);
+        if (n == 0) return res;
         res.push_back(1);
         int curr = 1;
         for (int i = 2; i <= n; ++i) {
